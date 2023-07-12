e00bc1d15908ff5b253d8b8b0d4fd4f88025f4d9
Alan Beck <kalanbeck@Skunk>
2023-07-11 22:26:04 -0400

    Favorites page & Popular page

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,19 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from './pages/Root';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-// import Main from './pages/Main';
+import Main from './pages/Main';
+import Popular from './pages/Popular';
+import Favorites from './pages/Favorites';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     children: [
-      // { path: "/", element: <Home /> },
       { path: "/", element: <Login />},
+      { path: "/favorites", element: <Favorites />},
       { path: "/main", element: <Main />},
+      { path: "/popular", element: <Popular />},
       { path: "/signup", element: <Signup />}
     ],
   },
---
+++ src/pages/Favorites.tsx
@@ -0,0 +1,59 @@
+import { useLocation } from "react-router-dom";
+import React, { useEffect, useState, useRef } from "react";
+import DrinkCard from "../components/DrinkCard";
+
+interface Drink {
+  idDrink: string;
+  strDrink: string;
+  strDrinkThumb: string;
+}
+
+export default function Favorites() {
+  const formData = useLocation().state;
+  const [isLoading, setIsLoading] = useState(true);
+  const [cardsData, setCardsData] = useState<Drink[]>([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      `/api/drinks`,
+      {
+        method: "GET",
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setCardsData([]);
+
+      //   {
+      //     "id": 5,
+      //     "name": "Bijou",
+      //     "image": "https://www.thecocktaildb.com/images/media/drink/vaukir1606772580.jpg",
+      //     "userid": 2,
+      //     "drinkid": "17254"
+      // }
+        
+        setCardsData(data.drinks);
+
+        setIsLoading(false);   
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, [formData]);
+
+  return (
+    <div className="cardDisplay">
+      <div className="card">
+          {cardsData.map((drink, index) => (
+            <DrinkCard
+              key={index}
+              name={drink.strDrink}
+              imgUrl={drink.strDrinkThumb}
+              id={drink.idDrink}
+            />
+          ))}
+        </div>
+    </div>
+  );
+}
